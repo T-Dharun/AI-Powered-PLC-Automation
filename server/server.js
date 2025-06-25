53ad71fd24726f82e467e0aa4f581bb7c8508ded
DHARUN  T <119958932+T-Dharun@users.noreply.github.com>
2025-06-25 15:37:26 +0530

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const app = express();
 
 const generative = require('./AI_Model/generate_program');
 const register=require('./routes/registerRoutes');
+const upload=require('./routes/uploadRoutes');
 
 
 const port=process.env.PORT || 3001;
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/generate", generative);
 app.use("/api",register);
+app.use("/api",upload);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
